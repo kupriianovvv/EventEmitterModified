d4b9493976be2923436ccb019a0dda384c6d81e9
kupriianovvv <kupriianovvv@gmail.com>
2023-01-18 09:42:57 +0300

    add capture handlers

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 class EventEmitter {
     handlers = {};
+    handlersCapture = {}
+
+    constructor(parent) {
+        this.parent = parent;
+        this.on.capture = (eventName, handler) => {
+            this.addHandler(eventName, handler, true);
+        }
+    }
 
     on(eventName, handler) {
-        if (this.handlers[eventName]) {
-            this.handlers[eventName].push(handler);
+        return this.addHandler(eventName, handler);
+    }
+
+    addHandler(eventName, handler, capture = false) {
+        const handlers = capture ? this.handlersCapture : this.handlers;
+        if (handlers[eventName]) {
+            handlers[eventName].push(handler);
             return handler;
         }
-        this.handlers[eventName] = [handler];
+        handlers[eventName] = [handler];
         return handler;
+
     }
     off(eventName, handler) {
         if (handler !== undefined) {
@@ -18,6 +32,19 @@ class EventEmitter {
 
     }
     emit(eventName) {
-        this.handlers[eventName].forEach(handler => handler())
+        this.handlers[eventName].forEach(handler => handler());
+        this.handlersCapture[eventName].forEach(handler => handler())
     }
 }
+
+const emitter = new EventEmitter();
+
+emitter.on("hoba", () => console.log("bubble hoba1"));
+emitter.on("hoba", () => console.log("bubble hoba2"));
+emitter.on("heya", () => console.log("bubble heya"));
+
+emitter.on.capture("hoba", () => console.log("capture hoba1"));
+emitter.on.capture("hoba", () => console.log("capture hoba2"));
+emitter.on.capture("heya", () => console.log("capture heya"));
+
+emitter.emit("hoba")
